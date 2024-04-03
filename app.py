ab711c0ce3f578c554b848182f2624ff491d27db
Tom Lebreton <tom.lebreton@supdevinci-edu.fr>
2024-04-03 16:49:14 +0200

    Modify app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Welcome to the Flask CI/CD Demo! This is V5!"
+    return "Welcome to the Flask CI/CD Demo! This is V6!"
 @app.route("/new-deployment")
 def new_deployment():
     return "New deployment from CI/CD"
